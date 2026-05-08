ae13c55b4db79d224d3b43b28d7b811250ff1ac3
Murilo Gomes da Rocha <murilo.gomes.rocha@escola.pr.gov.br>
2026-05-08 13:57:27 +0000

    alteração de arquivo

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ function mostrarInfo(tipo) {
     const detalhe = document.getElementById('detalhe');
     
     const mensagens = {
-        'energia': "☀️ <strong>Eficiência Energética:</strong> Painéis ampliados para sustentar uma estufa maior e mais tecnológica.",
-        'biologico': "🐞 <strong>Super Safra:</strong> Graças ao manejo sustentável, nossos galhos estão carregados com cachos de tomates saudáveis!",
-        'agua': "💧 <strong>Hidratação Total:</strong> Sistema de irrigação que cobre toda a extensão da estufa, garantindo água para todos os frutos.",
-        'pessoas': "🧺 <strong>Colheita Recorde:</strong> Com mais tomates por galho, nossos cestos ficam cheios rapidamente. É o agro forte em ação!"
+        'energia': "☀️ <strong>Energia Solar:</strong> Sustenta toda a nossa produção de forma limpa.",
+        'biologico': "🐞 <strong>Cachos de Tomate:</strong> Manejo sustentável gera plantas carregadas de frutos saudáveis.",
+        'agua': "💧 <strong>Irrigação:</strong> Gotejamento de precisão para economizar água.",
+        'pessoas': "🧺 <strong>Novo Cesto de Colheita:</strong> Mudamos para o modelo 'Balaio Arredondado', um tipo tradicional de vime marrom claro que protege melhor os tomates durante a colheita manual."
     };
 
     detalhe.innerHTML = mensagens[tipo];
